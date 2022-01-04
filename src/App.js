38404ce5a92cfab91fcc3033737ce35a306cf16c
vineetkolate <vineetvijay76@gmail.com>
2022-01-04 14:55:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+export default function app() {
+  return (
+    // <div className="bg-primary text-light p-4">
+    //   <div>Hello World</div>
+    // </div>
+
+    // <div className="beautify">
+    //   <div>Hello World</div>
+    // </div>
+
+    <div style={{ color: "red", background: "yellow" }}>
+      <div>Hello World</div>
+
+    </div>
+  );
+
+}
